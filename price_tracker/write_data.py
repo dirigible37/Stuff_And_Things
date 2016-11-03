61a3f48ade8c1f8040f8ac07c1ac5dc169cf2861
dirigible37 <mohw011@gmail.com>
2016-11-03 01:01:44 +0000

    Updating writing function

--- price_tracker/write_data.py
+++ price_tracker/write_data.py
@@ -1,4 +1,12 @@
+s = "newegg"
+i = "1070"
+
+
+data = {'man' : 'msi', 'model':'ftw', 'datetime':'123456', 'price':'59.99'}
+
 def write_data (site_name, item_name, data_obj):
-	file_name = "/data/"+site_name+"/"+item_name
-	target = open(file_name, "a")
-	target.write(data_obj)
+	file_name = "./data/"+site_name+"/"+item_name
+	target = open(file_name, "w")
+	target.write(data_obj['man'] + " " + data_obj['model'] + " " + data_obj['price'] + " " + data_obj['datetime'])
+
+write_data(s, i, data)
