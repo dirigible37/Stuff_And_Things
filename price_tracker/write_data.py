dd17649b3da4f49679b86b0f8208344d8895110d
Dirigible37 <mohw011@gmail.com>
2017-05-05 02:16:54 +0000

    Cleaned Up the Repo

--- price_tracker/write_data.py
+++
@@ -1,4 +0,0 @@
-def write_out (site_name, item_name, data_obj):
-	file_name = "/home/wmohr/Stuff_And_Things/price_tracker/data/"+site_name+"/"+item_name
-	target = open(file_name, "a")
-	target.write(data_obj['man'] + " " + data_obj['model'] + " " + data_obj['price'] + " " + data_obj['datetime'] + "\n")
